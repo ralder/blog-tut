8c6162082986295f58ecf1759f5dd4b2bd337fbc
sergey.mishin <s.mishin@skyeng.ru>
2021-03-09 01:19:48 +0300

    Add link for comment deleting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to article_path(@article)
+  end
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
